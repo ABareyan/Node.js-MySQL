9f8fcdf00edaed051d3f619224f27fdf1c7cba52
Arman Bareyan <abareyan@gmail.com>
2019-10-03 13:22:06 -0600

    Update DB

--- Part.sql
+++ Part.sql
@@ -1,4 +1,3 @@
-
 DROP DATABASE IF EXISTS bamazong_db;
 
 CREATE DATABASE bamazong_db;
@@ -9,8 +8,18 @@ CREATE TABLE product (
 	item_id INT AUTO_INCREMENT NOT NULL,
     product_name VARCHAR(33) NOT NULL,
     department_name VARCHAR(55) NOT NULL,
-    price DECIMAL(12, 4) NOT NULL,
+    price DECIMAL(12, 2) NOT NULL,
     stock_quantity DECIMAL(12) NOT NULL,
+    product_sales DECIMAL (12, 2),
     PRIMARY KEY (item_id)
-)
+);
 
+
+CREATE TABLE departments (
+	department_id INT AUTO_INCREMENT NOT NULL,
+    department_name VARCHAR(33) NOT NULL,
+    over_head_costs DECIMAL(12, 2) NOT NULL,
+    product_sales DECIMAL (12, 2),
+    total_profit DECIMAL (12 ,2),
+    PRIMARY KEY (department_id)
+);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,7 +16,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "<yourRootPassword>",
+    password: "yourpasswordhere",
     database: "bamazong_db"
 });
 
